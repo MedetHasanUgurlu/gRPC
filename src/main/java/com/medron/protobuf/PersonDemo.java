004c9b8d05a67948a23ed70b6497f0947c5556b2
TCMUGURLU <medet.ugurlu@atmosware.turkcell.com.tr>
2023-09-21 20:51:09 +0300

    JSON VS PROTO

--- src/main/java/com/medron/protobuf/PersonDemo.java
+++ src/main/java/com/medron/protobuf/PersonDemo.java
@@ -3,9 +3,18 @@ package com.medron.protobuf;
 
 import com.medron.models.Person;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class PersonDemo {
-    public static void main(String[] args) {
-        Person.newBuilder().setName("Metehan").setAge(27).build();
+    public static void main(String[] args) throws IOException {
+        final Person metehan = Person.newBuilder().setName("Metehan").setAge(27).build();
+        Path path = Paths.get("sam.yer");
+//        Files.write(path,metehan.toByteArray());
+        byte[] bytes = Files.readAllBytes(path);
 
     }
 }
